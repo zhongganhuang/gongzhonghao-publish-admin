2bca2d3d903583d8bc2dfcac7737232f8b75850c
Your Name <you@example.com>
2020-11-15 13:54:56 +0800

    用户登录-页面布局-处理完成

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,6 @@
   <div id="app">
     <router-view/>
 
-    <el-button type="success">成功按钮</el-button>
-
-    <el-input-number 
-    v-model="num" 
-    :min="1" 
-    :max="10" 
-    label="描述文字">
-    </el-input-number>
-
   </div>
 </template>
 
@@ -18,9 +9,7 @@
 export default {
   name:"App",
   data(){
-    return{
-      num:6
-    }
+    return{}
   }
 }
 </script>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,82 @@
 <template>
-    <div class="login-container">登录页面</div>
+  <div class="login-container">
+
+    <div class="login-head"></div>
+
+    <el-form class="login-form" ref="form" :model="user" >
+      <el-form-item >
+        <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
+      </el-form-item>
+
+
+      <el-form-item >
+        <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
+      </el-form-item>
+
+      <el-form-item >
+        <el-checkbox v-model="checked">我已经阅读并同意用户协议和政策隐私</el-checkbox>       
+      </el-form-item>    
+
+      <el-form-item>
+        <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'LoginIndex',
-    components:{},
-    props:{},
-    data(){
-        return{}
+  name: "LoginIndex",
+  components: {},
+  props: {},
+  data() {
+    return {
+      user: {
+          mobile:'',//手机号
+          code:''//验证码
+      },
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    onSubmit() {
+      console.log("submit!")
     },
-    computed:{},
-    watch:{},
-    created(){},
-    mounted(){},
-    methods:{},
-}
+  },
+};
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-container{
+    position:fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background:url("./login_bg.jpg") no-repeat;
+    //背景填充模式
+    background-size: cover;
+
+    .login-head{
+        width:300px;
+        height: 57px;
+        margin-bottom: 30px;
+        background: url("./logo_index.png") no-repeat;
+    }
+    .login-form{
+        background-color: #fff;
+        padding:50px;
+        min-width: 300px;
+        .login-btn{
+            width: 100%;
+        }
+    }
+}
+</style>
