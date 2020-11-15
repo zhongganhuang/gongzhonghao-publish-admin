0cd37d89d3b5f68abb80c515cb6dd9099d3f3915
Your Name <you@example.com>
2020-11-15 12:48:05 +0800

    用户登录-创建页面组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+
+    <el-button type="success">成功按钮</el-button>
+
+    <el-input-number 
+    v-model="num" 
+    :min="1" 
+    :max="10" 
+    label="描述文字">
+    </el-input-number>
+
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name:"App",
+  data(){
+    return{
+      num:6
     }
   }
 }
+</script>
+
+<style lang="less">
 </style>
---
+++ src/utils/request.js
@@ -0,0 +1,10 @@
+//基于axios封装请求模块
+import axios from 'axios'
+
+//创建一个axios实例，说白了就是复制一个axios。通过这个实例区发请求，把需要的配置配置给这个实例来处理
+const request= axios.create({
+    baseURL:'http://ttapi.research.itcast.cn/mp/'  //请求的基础路径
+})
+
+//到处请求方法
+export default request 
